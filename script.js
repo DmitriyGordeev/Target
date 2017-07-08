aeab58a163409175e7a77dd59483b907c51f8f74
Dima <dima@dima.com>
2017-07-08 19:42:30 +0300

    bottom row

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-var a = Snap(".can");
-var c = a.circle(50, 50, 10);
-var r = a.rect(100, 100, 20, 20);
 
-// var g = a.group(c, r);
-// g.attr({ fill: "green", background: "yellow" }).drag();
+jQuery(document).ready(function() {
 
-a.drag();
+
+});
+
+
