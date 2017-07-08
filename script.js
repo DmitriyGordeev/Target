4425121847461f38db01e7bce1914de79599d490
Dima <dima@dima.com>
2017-07-08 16:51:05 +0300

    rechange style

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var a = Snap(".can");
+var c = a.circle(50, 50, 10);
+var r = a.rect(100, 100, 20, 20);
 
-jQuery(document).ready(function() {
-}); 
+// var g = a.group(c, r);
+// g.attr({ fill: "green", background: "yellow" }).drag();
+
+a.drag();
