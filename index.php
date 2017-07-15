9860e6010ec011d0d9acee7f187b11fa9345b4ea
Dima <dima@dima.com>
2017-07-15 15:41:16 +0300

    created simple dynamic php page

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: dima
- * Date: 15.07.17
- * Time: 15:14
- */
+    class HtmlPage {
+
+        public function __construct($title, $centered_text) {
+            $this->title = $title;
+            $this->centered_text = $centered_text;
+        }
+        
+        /* methods: */
+        public function generate() {
+            return "<!DOCTYPE><html>".$this->head().$this->body()."</html>";
+        }
+        public function head() {
+            return "<head>
+                    <meta charset=\"UTF-8\">
+                    <title>".$this->title."</title>";
+        }
+        public function body() {
+            return "<body><p style='text-align: center; font-size: 30px'>".$this->centered_text."</p></body>";
+        }
+
+        /* properties: */
+        public $title;
+        public $centered_text;
+    }
+
+    $htmlPageObject = new HtmlPage("This is title", "This is centered text");
+    echo $htmlPageObject->generate();
+
