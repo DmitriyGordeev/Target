0c709cab8b540b0f5b2dd06ce7754e4c47e2dfda
Dimas <gordeev.dmitriy@physics.msu.ru>
2017-07-22 20:13:44 +0300

    simple user object

--- test_database.php
+++ test_database.php
@@ -1,12 +1,34 @@
 <?php
 
+/* database presets: */
+$db_host = "127.0.0.1";
+$db_user = "root";
+$db_pass = "123";
+$db_name = "goal_service";
+
 class UserPreferences {
+    public function __construct($id, $name, $email) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public $id;
+    public $name;
+    public $email;
 
 }
 
 
 /* testing simple database interaction: */
-$mysqli = mysqli_connect("127.0.0.1", "root", "123", "goal_service");
+$mysqli = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if($mysqli == false) {
+    echo "Unable to connect to database: mysqli == false";
+    exit();
+}
+
 $res = mysqli_query($mysqli, "select * from users");
 $row = mysqli_fetch_assoc($res);
-echo $row['id']." : ".$row['name']." : ".$row['email'];
+
+$up = new UserPreferences($row['id'], $row['name'], $row['email']);
+echo json_encode($up);
