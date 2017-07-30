ed4d3fa064cf7bea72f25b69d13bed717b1b34bc
dimas <user@user.com>
2017-07-30 13:39:47 +0300

    test php script

--- login.php
+++ login.php
@@ -1,12 +1,29 @@
 <?php
 
+// TODO: must be replaced with global params from database_settings.php:
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "db_test";
+// --------------------------------------------------------------------
+
+
 /* testing simple database interaction: */
-$mysqli = mysqli_connect("localhost", "root", "", "db_test");
+$mysqli = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if($mysqli == false) {
     echo "Unable to connect to database: mysqli == false";
     exit();
 }
 
-$result = $mysqli->query("select * from users");
+
+// exit if post unsuccessful:
+if(!isset($_POST)) {
+    echo "error: unable to receive email";
+    exit();
+}
+
+$result = $mysqli->query("select * from users where id=1");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo $row["id"]." = ".$row["name"];
+echo $row["name"];
+
+
