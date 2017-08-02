392fb8206cc0916defabfd550ea4de7942e1f154
dimas <user@user.com>
2017-08-02 09:47:46 +0300

    fixed query

--- retreive_goal_info.php
+++ retreive_goal_info.php
@@ -8,8 +8,7 @@
 session_start();
 include_once "database_settings.php";
 
-//$query_result = $mysqli->query("select user_goal_info from users where user_email='".$_SESSION["user_email"]."'");
-$query_result = $mysqli->query("select user_goal_info from users where email=\"dm@dm.ru\"");
+$query_result = $mysqli->query("select user_goal_info from users where email='".$_SESSION["user_email"]."'");
 
 $query_data = $query_result->fetch_array(MYSQLI_ASSOC);
 echo $query_data["user_goal_info"];
