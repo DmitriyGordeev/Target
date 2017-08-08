8807c2822ff8855375292295440292cf730f78be
dimas <user@user.com>
2017-08-08 07:52:48 +0300

    start / end dates in database's jsom

--- insert_user.php
+++ insert_user.php
@@ -8,11 +8,11 @@ $mysqli->query("update users SET date='".$_POST["datepicker"]."' where email='".
 $mysqli->query("update users SET penalty='".$_POST["ta-penalty"]."' where email='".$_SESSION["user_email"]."'");
 $mysqli->query("update users SET is_set=1 where email='".$_SESSION["user_email"]."'");
 
-$currentDateTime = date("d F Y H:m:s");
+$currentDateTime = date("d F Y");
 $mysqli->query("update users SET begin_date='".$currentDateTime."' where email='".$_SESSION["user_email"]."'");
 
 // default empty json:
-$default_goal_info = "{\"A\":{\"date\":\"\",\"description\":\"\",\"tasklist\":[]},\"alpha\":{\"date\":\"\",\"description\":\"\",\"tasklist\":[]},\"betta\":{\"date\":\"\",\"description\":\"\",\"tasklist\":[]},\"gamma\":{\"date\":\"\",\"description\":\"\",\"tasklist\":[]},\"B\":{\"date\":\"\",\"description\":\"\",\"tasklist\":[]}}";
+$default_goal_info = "{\"A\":{\"date\":\"".$currentDateTime."\",\"description\":\"\",\"tasklist\":[]},\"alpha\":{\"date\":\"\",\"description\":\"\",\"tasklist\":[]},\"betta\":{\"date\":\"\",\"description\":\"\",\"tasklist\":[]},\"gamma\":{\"date\":\"\",\"description\":\"\",\"tasklist\":[]},\"B\":{\"date\":\"".$_POST["datepicker"]."\",\"description\":\"\",\"tasklist\":[]}}";
 $mysqli->query("update users SET user_goal_info='".$default_goal_info."' where email='".$_SESSION["user_email"]."'");
 
 
